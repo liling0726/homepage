63da29790f12f3bc3fb59eaa14098c67e09c9dea
liujie <835001542@qq.com>
2015-03-30 20:20:55 +0800

    刘杰

--- src/main/java/swust/homepage/controller/IndexController.java
+++ src/main/java/swust/homepage/controller/IndexController.java
@@ -3,6 +3,7 @@ package swust.homepage.controller;
 import java.util.List;
 
 import swust.homepage.model.Acad;
+import swust.homepage.model.User;
 
 import com.jfinal.core.ActionKey;
 import com.jfinal.core.Controller;
@@ -12,6 +13,7 @@ import com.jfinal.plugin.activerecord.Record;
 public class IndexController extends Controller {
 	@ActionKey("/")
 	public void index() {
+		setAttr("topUserCount", User.dao.topUserCount(9));//刘杰：我就测试一下返回的list对不对
 		render("/html/homePage.html");
 	}
 	
@@ -27,5 +29,12 @@ public class IndexController extends Controller {
 	public void acadName(){
 		List<Acad> list = Acad.dao.find("select * from acad");
 		renderJson("acad", list);
+	}
+	
+	/**
+	 * liujie
+	 */
+	public void topUserCount(){
+		renderJson("topUserCount", User.dao.topUserCount(9));//liujie
 	}
 }
--- src/main/java/swust/homepage/model/User.java
+++ src/main/java/swust/homepage/model/User.java
@@ -1,8 +1,19 @@
 package swust.homepage.model;
 
+import java.util.List;
+
 import com.jfinal.plugin.activerecord.Model;
 
 public class User extends Model<User> {
 	private static final long serialVersionUID = 1767655565354816718L;
 	public static final User dao = new User();
+	/**
+	 * @author 刘杰
+	 * @param count 要查询的个数
+	 * @return 点击量前count的List<User>
+	 */
+	public List<User> topUserCount(int count){
+		return find("SELECT * FROM `user` ORDER BY user_count DESC LIMIT 0,"+
+	String.valueOf(count));
+	}
 }
