f3afb264eef36de852f681010b8a38fdd0a375bf
Administrator <Administrator@YH-20140509OXSQ>
2015-04-02 12:11:08 +0800

    zengdan

--- src/main/java/swust/homepage/controller/AdminAdminInfoController.java
+++ src/main/java/swust/homepage/controller/AdminAdminInfoController.java
@@ -17,7 +17,7 @@ import swust.homepage.model.User;
 public class AdminAdminInfoController extends Controller{
 
 	public void index() {
-		render("/html/acadInfoMore.html");
+		render("/html/adminAdminInfo.html");
 	}
 	
 	public void adminAdminInfo(){//老师->专业->学院
@@ -29,11 +29,12 @@ public class AdminAdminInfoController extends Controller{
 			String userString = u.getStr("user_name");
 			List<Dept> deptList = Dept.dao.find("select dept_name, dept_acad_id from dept "
 					+"where dept_id = " + u.getInt("user_dept_id"));
-
 			String deptString = deptList.get(0).getStr("dept_name");
+			
 			List<Acad> acadList = Acad.dao.find("select acad_name from acad "
-					+"where acad_id = " + deptList.get(0).getInt("dept_acad_id"));
+					+"where acad_id = " + deptList.get(0).getInt("dept_acad_id"));			
 			String acadString = acadList.get(0).getStr("acad_name");
+			
 			Map<String, String> map1 = new HashMap<String, String>();
 			map1.put(deptString, acadString);
 			map.put(userString, map1);
