9a15d09e059edc10ecc4ef75434c67df11d0e6fb
mrbin6git <18381668329@163.com>
2015-04-18 16:54:00 +0800

    问题解决页面
    Signed-off-by: mrbin6git <18381668329@163.com>

--- src/main/webapp/js/adminHomePageQuestion.js
+++ src/main/webapp/js/adminHomePageQuestion.js
@@ -1,5 +1,26 @@
-$(document).ready(function(){
-  $(".panel-body div button").click(function(){
-    $("ul").append("Hello world!");
-  });
-});
+var myDate = new Date();
+$(document)
+		.ready(
+				function() {
+					 $("div").delegate("#delete×","click",function(){
+						 $(this).parent("li").remove();
+						  });
+					$(".panel-body div button").on("click",function() {
+						var text = $(this).parents(
+						".panel-body").find('textarea')
+						.val();
+				$(this)
+						.parents('.panel-body')
+						.find('ul')
+						.append("<li>"
+										+ text
+										+ "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<label id='delete×'>×</label><span>"
+										+myDate.getFullYear()
+										+"-"
+										+(myDate.getMonth()+1)
+										+"-"
+										+myDate.getDate()
+										+"</span>"
+										+"</li>");
+				});
+				});
