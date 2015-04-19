65b585771a5c20d1eb4b9f6e4392d98e8d27f4ee
liujie <835001542@qq.com>
2015-04-19 23:14:12 +0800

    管理员老师页面-根据关键词查询老师

--- src/main/java/swust/homepage/controller/AdminTeacherInfoController.java
+++ src/main/java/swust/homepage/controller/AdminTeacherInfoController.java
@@ -6,80 +6,93 @@ import swust.homepage.model.User;
 import com.jfinal.core.Controller;
 
 /**
- * 
  * @author 刘杰
- *
+ *管理员页面的老师页面的控制类
  */
 public class AdminTeacherInfoController extends Controller {
 
 	public void index() {
 		getPage();
 	}
+
 	/**
 	 * 传入要保存的user对象的信息（不包含user_id）
 	 */
 	public void save() {
-		if(getModel(User.class).save()){
-			renderJson("result","添加成功");
-		}
-		else{
-			renderJson("result","添加失败");
+		if (getModel(User.class).save()) {
+			renderJson("result", "添加成功");
+		} else {
+			renderJson("result", "添加失败");
 		}
 	}
+
 	/**
 	 * 传入user的id和要改变的信息
 	 */
-	public void update(){
-		if(getModel(User.class).update()){
-			renderJson("result","更新成功");
-		}
-		else{
-			renderJson("result","更新失败");
+	public void update() {
+		if (getModel(User.class).update()) {
+			renderJson("result", "更新成功");
+		} else {
+			renderJson("result", "更新失败");
 		}
 	}
+
 	/**
 	 * 传入要删除的user的id，格式 1-2-3-4删除多个
 	 */
-	public void delete(){
-		int success=0;
-		int fail=0;
-		for(int i=0,p=getParaToInt(i,-1);p!=-1;i++){
-			p=getParaToInt(i,-1);
-			if(p>=0){
-				if(User.dao.deleteById(p)){
+	public void delete() {
+		int success = 0;
+		int fail = 0;
+		for (int i = 0, p = getParaToInt(i, -1); p != -1; i++) {
+			p = getParaToInt(i, -1);
+			if (p >= 0) {
+				if (User.dao.deleteById(p)) {
 					success++;
-					System.out.println("User:id="+p+" 删除成功！");
-				}
-				else{
+					System.out.println("User:id=" + p + " 删除成功！");
+				} else {
 					fail++;
-					System.out.println("User:id="+p+" 删除失败！");
+					System.out.println("User:id=" + p + " 删除失败！");
 				}
 			}
 		}
-		renderJson("result","删除成功"+success+"个，失败"+fail+"个");
+		renderJson("result", "删除成功" + success + "个，失败" + fail + "个");
 	}
+
 	/**
 	 * 接受一个参数，返回对应id的user实例，若不存在返回“用户不存在”
 	 */
-	public void edit(){
-		User user=User.dao.findById(getParaToInt());
-		if(user==null){
-			renderJson("user","用户不存在");
-		}
-		else{
-			renderJson("user",user);
+	public void edit() {
+		User user = User.dao.findById(getParaToInt());
+		if (user == null) {
+			renderJson("user", "用户不存在");
+		} else {
+			renderJson("user", user);
 		}
 	}
+
 	/**
-	 * 获取一个页面的老师集合，参数/a-b		获取第a页的老师，每页有b个老师
+	 * 获取一个页面的老师集合，参数/a-b 获取第a页的老师，每页有b个老师
 	 */
-	public void getPage(){
-		renderJson("userPage", User.dao.paginate(getParaToInt(0, 1), getParaToInt(1, 10)));
+	public void getPage() {
+		renderJson("userPage",
+				User.dao.paginate(getParaToInt(0, 1), getParaToInt(1, 10)));
 	}
+
 	/**
 	 * 根据学院Id查专业，如传参数Id=2,找出这个学院下的所有专业名、专业Id
 	 */
-	public void findDeptByAcadId(){
-		renderJson("deptList",Dept.dao.findByAcadId(getParaToInt(0,1)));
+	public void findDeptByAcadId() {
+		renderJson("deptList", Dept.dao.findByAcadId(getParaToInt(0, 1)));
+	}
+
+	/**
+	 * 根据关键字查询
+	 * key 关键字
+	 * pageNumber 页码
+	 * pageSize 每页最多条数
+	 */
+	public void findUserByKey() {
+		renderJson("keyUserPage", User.dao.paginateByKey(getPara("key"),
+				getParaToInt("pageNumber"), getParaToInt("pageSize")));
 	}
 }
--- src/main/java/swust/homepage/model/User.java
+++ src/main/java/swust/homepage/model/User.java
@@ -21,8 +21,8 @@ public class User extends Model<User> {
 
 	/**
 	 * @author 刘杰
-	 * @param pageNumber查询页数
-	 * @param pageSize每页的数量
+	 * @param pageNumber 页码
+	 * @param pageSize 每页最大条数
 	 * @return 含有用户 （user表内）所有信息以及专业名，学院名的对象集
 	 */
 	public Page<User> paginate(int pageNumber, int pageSize) {
@@ -44,6 +44,24 @@ public class User extends Model<User> {
 		System.out.println(list.size());
 		return list;
 		
+	}
+	/**
+	 * @author 刘杰
+	 * @param key 关键字
+	 * @param pageNumber 页码
+	 * @param pageSize 每页最大条数
+	 * @return 符合查找条件的集合
+	 */
+	public Page<User> paginateByKey(String key,int pageNumber, int pageSize) {
+		System.out.println("按关键字查找ing...Key="+key+"  页码="+String.valueOf(pageNumber)+"  最大条数="+String.valueOf(pageSize));
+		return paginate(pageNumber, pageSize, 
+				"select `user`.*,acad.acad_name,dept.dept_name",
+				" from `user`,acad,dept where"
+				+ " (`user`.user_name like '%"+key+"%'"
+				+ " or `user`.user_num like '%"+key+"%'"
+				+ " or acad_name like '%"+key+"%'"
+				+ " or dept.dept_name like '%"+key+"%')"
+				+ " and `user`.user_dept_id = dept.dept_id and dept.dept_acad_id = acad.acad_id");
 	}
 
 }
