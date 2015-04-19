0b4e0a73ce3fcd776878f5d86ca2a68669ed47d8
mrbin6git <18381668329@163.com>
2015-04-19 22:02:52 +0800

    mrbin

--- src/main/webapp/js/adminAcadInfo.js
+++ src/main/webapp/js/adminAcadInfo.js
@@ -1,3 +1,4 @@
+var data=new Array();
 $(function(){
 	
 	$("table tr td:nth-child(2)").mouseover(function(e){
@@ -26,4 +27,45 @@ $(function(){
 		y:ev.clientY + document.body.scrollTop - document.body.clientTop
 		};
 		}
-})
+	
+	
+	$.ajax({
+		type:"post",
+		content:"application/x-www-from-urlencoded;charset=UTF-8",
+		dataType:"json",
+		url:"/adminacadinfo/index",
+		/*data:"user.user_num="+userNum+"&user.user_name="+userName+"&user.user_dept_id="+userDeptId,*/
+		async:false,
+		success:function(result){
+			data=result.result;
+			alert(data.length);
+		for(var i = 0;;i++)
+			{
+				data=result.result[i];
+				if(data[0]==undefined)
+					break;
+				$("#maintable").append(
+						"<tr><td>"+i+"</td><td>"+data[0].dept_name+"</td><td><ul id=\"deptli"+i+"\"class=\"classInfo\"></ul></td>" +
+						"<td style=\"width: 100px;\"><div class=\"buttonGroup\"><button class=\"btn btn-danger btn-xs\" " +
+						"data-toggle=\"modal\"data-target=\"#insertModal\">添加</button>" +
+						"<button class=\"btn btn-info btn-xs\">删除</button></div></td></tr>"
+				
+				);
+				for(var j = 1;j<data.length;j++)
+				{
+					
+					$("#deptli"+i).append("<li><input type=\"checkbox\">&nbsp;&nbsp;<a>"+data[j].dept_name+"</a></li>");
+				}
+			
+			}
+		},
+		error:function(e){
+			console.log("错误："+e.message);
+		}
+	});
+	$("#addacad").click(function(){
+		/*var addacadname=$(this).parents("modal-body").find("input").val();*/
+		/*alert(addacadname);*/
+		
+	});
+});
