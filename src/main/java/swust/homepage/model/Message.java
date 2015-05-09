89c9665db61058fce8047f892215cf34b384acef
syca <sycamore0516@gmail.com>
2015-05-09 22:28:04 +0800

    留言Model

---
+++ src/main/java/swust/homepage/model/Message.java
@@ -0,0 +1,8 @@
+package swust.homepage.model;
+
+import com.jfinal.plugin.activerecord.Model;
+
+public class Message extends Model<Message> {
+	private static final long serialVersionUID = 3845170855212521938L;
+	public static final Message dao = new Message();
+}
