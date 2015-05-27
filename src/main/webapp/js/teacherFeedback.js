4a60aeb42cbab4b5b6526c5c1de0cbbbd51372c2
mrbin6git <18381668329@163.com>
2015-05-27 17:23:04 +0800

    合teacherMessage页面

--- src/main/webapp/js/teacherFeedback.js
+++ src/main/webapp/js/teacherFeedback.js
@@ -1,41 +1,190 @@
 var myDate = new Date();
-$(document).ready(
-	function(){
-$("li").delegate("span[name='span1']", "click", function() {
-	$(this).parent().remove();
-});
-$("#confirm")
-		.on(
-				"click",
+var currentPage, // 当前页
+totalNum, // 一页总条数
+maxPage;// 最大页数
+$(document)
+		.ready(
 				function() {
-					var text = $("#textarea1").val();
-					if(text=="")
-						alert("没有文字！");
-					else
-						{
-					$("#suggestion").append("<li><a href=\"#\">" 
-							+text
-							+"<span class=\"label label-info\">已解决</span><span>"
-							+myDate.getFullYear()
-							+"-"
-							+(myDate.getMonth()+1)
-							+"-"
-							+myDate.getDate()
-							+"</span></a></li>"
-							);
-						}
-					$("#textarea1").val("");
-				});
-$("#suggestion li").find("span[name='deleteSpan']").hide();
-$("#suggestion li").mouseover(function(){
-	$(this).find("span[name='deleteSpan']").show();
-})
-$("#suggestion li").mouseleave(function(){
-	$(this).find("span[name='deleteSpan']").hide();
-})
-$("span[name='deleteSpan']").click(function(){
-	alert("确认删除？");
-});
-});
+					currentPage=$("#currentPage").val();
+					 maxPage=$("#max").val();
+					 //alert(maxPage);
+					 if(currentPage=="undefined"||currentPage=="")
+						 currentPage=1;
+					 //初始化
+						initial();
+					
+					$("li").delegate("span[name='span1']", "click", function() {
+						$(this).parent().remove();
+					});
+					$("#confirm")
+							.on(
+									"click",
+									function() {
+										var text = $("#textarea1").val();
+										if (text == "")
+											alert("没有文字！");
+										else {
+											$("#suggestion")
+													.append(
+															"<li><a href=\"#\">"
+																	+ text
+																	+ "<span class=\"label label-info\">已解决</span><span>"
+																	+ myDate
+																			.getFullYear()
+																	+ "-"
+																	+ (myDate
+																			.getMonth() + 1)
+																	+ "-"
+																	+ myDate
+																			.getDate()
+																	+ "</span></a></li>");
+											$
+													.ajax({
+														type : "post",
+														content : "application/x-www-from-urlencoded;charset=UTF-8",
+														dataType : "json",
+														url : "../teacherFeedback/save",
+														data : {
+															feedbackString : text
+														},
+														async : false,
+														success : function(
+																result) {
+															alert(result.result);
+															window.location
+																	.reload();
+														},
+														error : function(e) {
+															console
+																	.log("错误："
+																			+ e.message);
+														}
+													});
+										}
 
-//添加过后要刷新！！！！！！！！！！！！！！！！！！！
+										$("#textarea1").val("");
+									});
+					$("#suggestion li").find("span[name='deleteSpan']").hide();
+					$("#suggestion li").mouseover(function() {
+						$(this).find("span[name='deleteSpan']").show();
+					})
+					$("#suggestion li").mouseleave(function() {
+						$(this).find("span[name='deleteSpan']").hide();
+					})
+					$("span[name='deleteSpan']").click(function() {
+						alert("确认删除？");
+					});
+				});
+/*
+ * 初始化页面
+ */
+function initial() {
+	$.ajax({
+		type : "post",
+		content : "application/x-www-form-urlencoded;charset=UTF-8",
+		dataType : "json",
+		url : "../teacherFeedback/" + currentPage + "-" + maxPage,
+		async : "false",
+		success : function(result) {
+
+		},
+		error : function(e) {
+			console.log("错误：" + e);
+		}
+	});
+}
+// 添加过后要刷新！！！！！！！！！！！！！！！！！！！
+//跳转
+$("#goto").bind("click",function(){
+	var gotopage=$("#gotoPage").val();
+	if(!gotopage.match("^\\d+$")){//判断是否为数字
+		alert("请输入规范的页码");
+		return;
+	}
+	if(gotopage<1||gotopage>totalNum)
+	{
+		alert("超出总页数！");
+		return false;
+	}
+	else
+	{
+		currentPage=gotopage;
+		//调用查询
+		if(keyWord==""||keyWord==null)
+		initial();
+		else searchByKey(keyWord);
+	}
+
+});
+//	下一页
+$("#pageforward").bind("click",function(){
+	
+	if(currentPage<totalNum)
+	{
+	currentPage=parseInt(currentPage)+1;
+	//调用查询
+	if(keyWord==""||keyWord==null)
+	initial();
+	else searchByKey(keyWord);
+	}
+	else{
+		alert("超出总页数");
+		return;
+	}
+	
+});
+//	上一页
+$("#pagebackward").bind("click",function(){
+	
+	if(currentPage>1)
+	{currentPage=parseInt(currentPage)-1;
+	//调用查询
+	if(keyWord==""||keyWord==null)
+	initial();
+	else searchByKey(keyWord);
+	}
+	else{
+		alert("小于总页数");
+		return;
+	}
+
+});
+//点击首页，显示第一页数据
+$("#firstPage").bind("click",function(){
+	if(currentPage==1)
+		{
+		alert("已经第一页了");
+		return false;
+		}
+	else{
+	currentPage=1;
+	//调用查询
+	if(keyWord==""||keyWord==null)
+	initial();
+	else searchByKey(keyWord);
+	}
+});
+//点击末页，显示最后页数据
+$("#lastPage").bind("click",function(){
+	if(currentPage==totalNum)
+		{
+		alert("已经是最后页了");
+		return false;
+		}
+	else{
+	currentPage=totalNum;
+	//调用查询
+	if(keyWord==""||keyWord==null)
+	initial();
+	else searchByKey(keyWord);
+	}
+});
+//每页显示页数
+$("#max").bind("change",function(){
+	 maxPage=$("#max").val();
+	 //alert(maxPage);
+	//调用查询
+		if(keyWord==""||keyWord==null)
+		initial();
+		else searchByKey(keyWord);
+});
--- src/main/webapp/js/teacherMessage.js
+++ src/main/webapp/js/teacherMessage.js
@@ -1,26 +1,170 @@
+var currentPage, // 当前页
+totalNum, // 最大页数
+maxPage;// 一页总条数
 $(document).ready(function(){
-	$("span[name='saw']").each(function(){
+	currentPage=$("#currentPage").val();
+	 maxPage=$("#max").val();
+	 //alert(maxPage);
+	 if(currentPage=="undefined"||currentPage=="")
+		 currentPage=1;
+	 //初始化
+		initial();
+
+/*
+ * 初始化页面
+ */
+function initial() {
+	$.ajax({
+		type : "post",
+		content : "application/x-www-form-urlencoded;charset=UTF-8",
+		dataType : "json",
+		url : "../teacherMessage/" + currentPage + "-" + maxPage,
+		async : "false",
+		success : function(result) {
+			totalNum=result.teacherMessage.totalPage;
+			var data=result.teacherMessage.list;
+			var html="";
+			for(var i = 0;i<data.length;i++){
+				if(i==data.length-1)
+					{
+					html+="<li><div class=\"noborder\"><div><a>"
+						+data[i].user_name
+						+":</a>&nbsp;&nbsp;&nbsp;<span>" 
+						+data[i].message_content
+						+"</span></div><div><span name=\"saw\">"
+						+"未查看" 
+						+"</span><span>"
+						+"2015-5-24 20:02" 
+						+"</span><span>"
+						+data[i].message_email
+						+"</span></div></div></li>";
+					}
+				else{
+					html+="<li><div class=\"liborder\"><div><a>"
+						+data[i].user_name
+						+":</a>&nbsp;&nbsp;&nbsp;<span>" 
+						+data[i].message_content
+						+"</span></div><div><span name=\"saw\">"
+						+"未查看" 
+						+"</span><span>"
+						+"2015-5-24 20:02" 
+						+"</span><span>"
+						+data[i].message_email
+						+"</span></div></div></li>";
+				}
+			}
+			$("#mainUl").html(html);
+			$("span[name='saw']").each(function(){
 				var spanHtml;
 				spanHtml=$(this).html();
 				if(spanHtml=="已查看")
 					{
-					$(this).css("color","#333");
+					$(this).css("color","DarkGray");
 					}
 				if(spanHtml=="未查看"){
 					$(this).css("color","red");
 				}
 	});
-	$("span[name='saw']").click(function(){
-		var spanHtml;
-		spanHtml=$(this).html();
-		if(spanHtml=="未查看")
-			{
-			$(this).html("已查看");
-			$(this).css("color","#333");
-			}
-		if(spanHtml=="已查看"){
-			$(this).html("未查看");
-			$(this).css("color","red");
+			$("span[name='saw']").click(function(){
+				var spanHtml;
+				spanHtml=$(this).html();
+				if(spanHtml=="未查看")
+					{
+					$(this).html("已查看");
+					$(this).css("color","DarkGray");
+					}
+				if(spanHtml=="已查看"){
+					$(this).html("未查看");
+					$(this).css("color","red");
+				}
+			});
+			
+			 $("#currentPage").html(result.teacherMessage.pageNumber);
+	    	 $("#totalPage").html(totalNum);
+		},
+		error : function(e) {
+			console.log("错误：" + e);
 		}
 	});
-})
+}
+
+// 添加过后要刷新！！！！！！！！！！！！！！！！！！！
+//跳转
+$("#goto").bind("click",function(){
+	var gotopage=$("#gotoPage").val();
+	if(!gotopage.match("^\\d+$")){//判断是否为数字
+		alert("请输入规范的页码");
+		return;
+	}
+	if(gotopage<1||gotopage>totalNum)
+	{
+		alert("超出总页数！");
+		return false;
+	}
+	else
+	{
+		currentPage=gotopage;
+		initial();
+	}
+
+});
+//	下一页
+$("#pageforward").bind("click",function(){
+	
+	if(currentPage<totalNum)
+	{
+	currentPage=parseInt(currentPage)+1;
+	alert(currentPage);
+	initial();
+	}
+	else{
+		alert("超出总页数");
+		return;
+	}
+	
+});
+//	上一页
+$("#pagebackward").bind("click",function(){
+	
+	if(currentPage>1)
+	{currentPage=parseInt(currentPage)-1;
+	initial();
+	}
+	else{
+		alert("小于总页数");
+		return;
+	}
+
+});
+//点击首页，显示第一页数据
+$("#firstPage").bind("click",function(){
+	if(currentPage==1)
+		{
+		alert("已经第一页了");
+		return false;
+		}
+	else{
+	currentPage=1;
+	initial();
+	}
+});
+//点击末页，显示最后页数据
+$("#lastPage").bind("click",function(){
+	if(currentPage==totalNum)
+		{
+		alert("已经是最后页了");
+		return false;
+		}
+	else{
+	currentPage=totalNum;
+	initial();
+	}
+});
+//每页显示页数
+$("#max").bind("change",function(){
+	 maxPage=$("#max").val();
+	 currentPage=1;
+	 alert(maxPage);
+	 initial();
+});
+});
