385fdd20d7dd3f9a111e851a89db1ee174b4e6e0
syca <sycamore0516@gmail.com>
2015-05-31 22:03:40 +0800

    增加根据id查询 栏目

--- src/main/java/swust/homepage/controller/TeacherColumnManageController.java
+++ src/main/java/swust/homepage/controller/TeacherColumnManageController.java
@@ -14,6 +14,7 @@ public class TeacherColumnManageController extends Controller {
 	/**
      * 新建栏目
      * 需要提供的字段 data_name(栏目名称) data_content(栏目内容) data_order(栏目排序) data_url(栏目别名)
+	 * data_type(栏目类型) data_nature(栏目性质) data_is_show(栏目是否显示 1 显示 2 不显示)
      * 至少需要提供 data_name(栏目名称)
      * url: /teacherColumnManage/save
 	 */
@@ -51,10 +52,24 @@ public class TeacherColumnManageController extends Controller {
         Page<Data> page = Data.dao.page(getParaToInt("pageNum"), getParaToInt("need"), getAttrForInt("data_user_id"));
         dataOrInfo(page.getList());
     }
+
+    /**
+     * 根据data_id查询
+     * 必须提供 data_id
+     * url: /teacherColumnManage/findById
+     */
+    public void findById() {
+        Data data =  Data.dao.findById(getParaToInt("data_id"));
+        if (data != null)
+            renderJson("result", data);
+        else
+            renderJson("result", "失败");
+    }
 	
 	/**
 	 * 更新栏目
-     * 这四个字段中任意的组合 data_name(栏目名称) data_content(栏目内容) data_order(栏目排序) data_url(栏目别名)
+     * 这几个字段中任意的组合 data_name(栏目名称) data_content(栏目内容) data_order(栏目排序) data_url(栏目别名)
+	 * data_type(栏目类型) data_nature(栏目性质) data_is_show(栏目是否显示 1 显示 2 不显示)
 	 * 但是必须提供 data_id
      * url: /teacherColumnManage/update
 	 */
@@ -65,7 +80,10 @@ public class TeacherColumnManageController extends Controller {
 		putIfExists(paraMap, "data_name");
 		putIfExists(paraMap, "data_content");
 		putIfExists(paraMap, "data_order");
-		putIfExists(paraMap, "data_parent");
+		putIfExists(paraMap, "data_url");
+        putIfExists(paraMap, "data_type");
+        putIfExists(paraMap, "data_nature");
+        putIfExists(paraMap, "data_is_show");
 		trueOrFalse(data.setAttrs(paraMap).update());
 	}
 
@@ -73,11 +91,14 @@ public class TeacherColumnManageController extends Controller {
 	/* ------------------------------------------------------------------------- */
 	private Data genData() {
 		Data data = new Data();
+        data.set("data_user_id", getSession().getAttribute("user_id"));
 		data.set("data_name", getPara("data_name"));
 		data.set("data_content", getPara("data_content"));
 		data.set("data_order", getParaToInt("data_order"));
 		data.set("data_url", getPara("data_url"));
-		data.set("data_user_id", getSession().getAttribute("user_id"));
+        data.set("data_type", getPara("data_type"));
+        data.set("data_nature", getPara("data_nature"));
+        data.set("data_is_show", getPara("data_is_show"));
 		data.set("data_update_time", new Date());
 		return data;
 	}
