e7dc10029e2939d3cf81bce4e0632fb503a8807a
Administrator <Administrator@YH-20140509OXSQ>
2015-07-07 10:47:26 +0800

    修改教师个人展示页面，renderJson改为render

--- src/main/java/swust/homepage/controller/TeacherPersonalShowController.java
+++ src/main/java/swust/homepage/controller/TeacherPersonalShowController.java
@@ -15,11 +15,18 @@ public class TeacherPersonalShowController extends Controller {
 	
 	public void index(){
 		setSessionAttr("user_id", 4);
-		renderJson("teacherPersonalShow",  BasicInfo.dao.teacherPersonalShow(getSessionAttr("user_id")));
+		//renderJson("teacherPersonalShow",  BasicInfo.dao.teacherPersonalShow(getSessionAttr("user_id")));
+		
+		setAttr("teacherBasicInfo", BasicInfo.dao.teacherPersonalShow(getSessionAttr("user_id")));
+		render("/html/teacherPersonalShow.html");
 	}
 	
+	//获取教师栏目内容
 	public void getTeacherDataContent(){
 		setSessionAttr("user_id", 4);
-		renderJson("teacherDataContent",  Data.dao.getDataContent(getSessionAttr("user_id")));
+		//renderJson("teacherDataContent",  Data.dao.getDataContent(getSessionAttr("user_id")));
+		
+		setAttr("teacherDataContent", BasicInfo.dao.teacherPersonalShow(getSessionAttr("user_id")));
+		render("/html/teacherPersonalShow.html");
 	}
 }
