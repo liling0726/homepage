2b79da4a66d3f490c40c232b2eb1f89863284604
mrbin6git <18381668329@163.com>
2015-07-15 09:10:51 +0800

    写了一点，传上去好更新

--- src/main/webapp/js/teacherIndex.js
+++ src/main/webapp/js/teacherIndex.js
@@ -40,4 +40,34 @@ $(function () {
             data: [ 50, 10, 39, 69, 10, 54, 70]
         }]
     });
+    	$.ajax({
+			type : "post",
+			content : "application/x-www-form-urlencoded;charset=UTF-8",
+			dataType : "json",
+			url : "../teacherIndex/" + 1 + "-" + 3,
+			async : "false",
+			success : function(result) {
+				var totalNum=result.teacherMessage.totalPage;
+				var data=result.teacherMessage.list;
+			var html= "";
+			for(var i = 0;i < data.length;i++){
+				html=html+"<tr><td>" 
+				+(i+1)
+				+"</td><td>"
+				+data[i].message_content
+				+"</td><td>"
+				+data[i].user_name
+				+"</td><td>" 
+				+data[i].message_email
+				+"</td> <td>" 
+				+data[i].message_submit_time
+				+"</td></tr>"
+			}
+			$("#messageTbody").html(html);
+		},
+		error : function(e) {
+			console.log("错误：" + e.message);
+		}
+	});
+    
 });				
