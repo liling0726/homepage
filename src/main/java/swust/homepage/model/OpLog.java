716517a283a06df5a4bf575f40f6ca2ee6905bb0
jinlong <sycamore0516@gmail.com>
2015-07-16 16:50:55 +0800

    teacherShowMore

--- src/main/java/swust/homepage/model/OpLog.java
+++ src/main/java/swust/homepage/model/OpLog.java
@@ -2,9 +2,16 @@ package swust.homepage.model;
 
 import com.jfinal.plugin.activerecord.Model;
 
-/**
- * Created by jinlong on 2015/6/26.
- */
+import java.util.List;
+
+/** Jin Long */
 public class OpLog extends Model<OpLog> {
     public static final OpLog dao = new OpLog();
+
+    public List<OpLog> get5Log() {
+        String sql = "SELECT user_name, op, time FROM op_log, user WHERE op_log.user_id=user.user_id ORDER BY time desc LIMIT 5";
+        return find(sql);
+    }
+
+
 }
