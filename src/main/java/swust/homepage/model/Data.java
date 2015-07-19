dac86e7c0312df63f255a22cf3d0c264df156348
Administrator <Administrator@YH-20140509OXSQ>
2015-07-19 19:14:50 +0800

    zd-老师个人展示页面，若传过来的id为空则设置为默认第一个页面；返回给前台的数据是根据order_id排序返回，若order_id为0则根据data_id排序

--- src/main/java/swust/homepage/model/Data.java
+++ src/main/java/swust/homepage/model/Data.java
@@ -3,7 +3,10 @@ package swust.homepage.model;
 import com.jfinal.plugin.activerecord.Model;
 import com.jfinal.plugin.activerecord.Page;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Data extends Model<Data> {
 	private static final long serialVersionUID = -27245496130704114L;
@@ -32,9 +35,20 @@ public class Data extends Model<Data> {
 	 *            teacherId, 对应教师的id
 	 * @return
 	 */
-	public List<Data> getDataName(int dataUserId) {
-		return find("select data_id, data_name from data where data_user_id = "
-				+ dataUserId);
+	public Map<Integer, String> getDataName(int dataUserId) {
+		Map<Integer, String> map = new HashMap<Integer, String>();
+		List<Data> listAll = new ArrayList<Data>();
+		List<Data> list = new ArrayList<Data>();
+		listAll = find("select data_id, data_name from data where data_user_id = "+dataUserId+" and data_order!=0 ORDER BY data_order");
+		list = find("select data_id, data_name from data where data_user_id = "+dataUserId+" and data_order=0 ORDER BY data_id");
+		for(int i=0; i<listAll.size(); i++){
+			map.put(listAll.get(i).getInt("data_id"), listAll.get(i).getStr("data_name"));
+		}
+		for(int i=0; i<list.size(); i++){
+			map.put(list.get(i).getInt("data_id"), list.get(i).getStr("data_name"));
+		}
+		//System.out.println(map);
+		return map;
 	}
 
 	/**
