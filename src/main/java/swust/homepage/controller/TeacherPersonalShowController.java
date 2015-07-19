06708d858a9505e53b116a8d141ea8de96a7c4cc
Administrator <Administrator@YH-20140509OXSQ>
2015-07-19 09:19:21 +0800

    zd-修改teacherPersonalShowController  获取data_id通过getParaToInt（）

--- src/main/java/swust/homepage/controller/TeacherPersonalShowController.java
+++ src/main/java/swust/homepage/controller/TeacherPersonalShowController.java
@@ -14,17 +14,14 @@ import com.jfinal.core.Controller;
 public class TeacherPersonalShowController extends Controller {
 	
 	public void index(){
-	//	int user_id=getSessionAttr("user_id");
 		setSessionAttr("user_id", 4);
-		//renderJson("teacherPersonalShow",  BasicInfo.dao.teacherPersonalShow(getSessionAttr("user_id")));
-		
 		setAttr("teacherBasicInfo", BasicInfo.dao.teacherPersonalShow(getSessionAttr("user_id")).get(0));
-		setAttr("teacherDataContent", Data.dao.getDataName(getSessionAttr("user_id")));
+		setAttr("teacherDataName", Data.dao.getDataName(getSessionAttr("user_id")));
 		render("/html/teacherPersonalShow.html");
 	}
 	
 	public void dataContent(){
-		setAttr("teacherDataContent", Data.dao.getDataContent(getParaToInt("data")));
+		setAttr("teacherDataContent", Data.dao.getDataContent(getParaToInt(1)));
 		render("/html/teacherPersonalShow.html");
 	}
 	
