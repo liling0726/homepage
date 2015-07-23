4e5238bad7038f18b42a064fe873565f07a0d7c8
mrbin6git <18381668329@163.com>
2015-07-23 21:39:07 +0800

    acadInfoMore页面

--- src/main/webapp/js/acadInfoMore.js
+++ src/main/webapp/js/acadInfoMore.js
@@ -7,15 +7,23 @@ $(function(){
 		async : false,
 		success : function(result) {
 			var data = result.acadInfoMore;
+			alert(data[0]);
 			var html = "";
-			var acad;
-			for(var i = 0;i<data.length;i++){
-				acad=data[i].acad_name;
-				if(data[i+1].acad_name==acad){
-					
+			for(var j=0;j<data.length;j++){
+			html+="<div class='col-md-3'><div class='panel panel-danger'><div class='panel-heading'>"
+				+"<center><label>"
+				+计算机科学与技术学院
+				+"</label></center></div><div class='panel-body'><table  class='table'><tr>";
+			for(var i = 0;i<data[j].length;i++){
+				html+="<td><a href='#'>"
+					+计算机工程系
+					+"</a></td>";
+				if(i==3){
+					html+="</tr><tr>";
 				}
 			}
-		
+			html+="</tr></table></div></div></div>";
+			}
 		},
 		error : function(e) {
 			console.log("错误：" + e.message);
