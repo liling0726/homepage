4bdfcd461f800185cf1f3fbf3535d664ce87005a
jinlong <sycamore0516@gmail.com>
2015-07-29 18:38:11 +0800

    操作日志查找日期判断改正 将结束日期加上一天进行判断

--- src/main/java/swust/homepage/model/OpLog.java
+++ src/main/java/swust/homepage/model/OpLog.java
@@ -2,12 +2,16 @@ package swust.homepage.model;
 
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Model;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 /** Jin Long */
 public class OpLog extends Model<OpLog> {
 
     public static final OpLog dao = new OpLog();
+    private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     /** 查找前5条操作日志 */
     public List<OpLog> get5Log() {
@@ -24,10 +28,19 @@ public class OpLog extends Model<OpLog> {
 
     /** 根据时间查找操作日志 分页 */
     public List<OpLog> findByTime(int page, int recordPerPage, String startTime, String endTime) {
+
         int start = (page - 1) * recordPerPage;
+
+        String endTimeM = endTime;
+        try {
+            endTimeM = sdf.format(sdf.parse(endTime).getTime() + 24 * 60 * 60 * 1000);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
         String sql = "SELECT user_name, op, time FROM op_log, user"
                 + " WHERE op_log.user_id=user.user_id AND time >= '" + startTime
-                + "' AND time <= '" + endTime
+                + "' AND time <= '" + endTimeM
                 + "' ORDER BY time desc LIMIT " + start + ", " + recordPerPage;
         return find(sql);
     }
@@ -37,9 +50,17 @@ public class OpLog extends Model<OpLog> {
     }
 
     public long countRecordByTime(String startTime, String endTime) {
+
+        String endTimeM = endTime;
+        try {
+            endTimeM = sdf.format(sdf.parse(endTime).getTime() + 24 * 60 * 60 * 1000);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
         String sql = "SELECT COUNT(*) FROM op_log"
                 + " WHERE time >= '" + startTime
-                + "' AND time <= '" + endTime + "'";
+                + "' AND time <= '" + endTimeM + "'";
         return Db.queryLong(sql);
     }
 
